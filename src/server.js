c52c103aebd9b83907bf8fab922d4903a56c31a5
coco <cocochanil5711@gmail.com>
2024-03-13 01:06:03 +0900

    server 4000

--- src/server.js
+++ src/server.js
@@ -5,5 +5,5 @@ const PORT = 4000;
 const app = express();
 
 app.listen(PORT, () => {
-  console.log(`Server listenting on port http://localhost:${PORT}`);
+  console.log(`Server listenting on port http://lhst:${PORT}`);
 });
